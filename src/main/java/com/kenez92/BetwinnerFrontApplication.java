a1e1dbf5858e0055b08ae24ee7419cebc8f60e52
Daniel Kaminski <kenez92@gmail.com>
2020-10-21 14:03:23 +0200

    Initial commit

---
+++ src/main/java/com/kenez92/BetwinnerFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kenez92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetwinnerFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetwinnerFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenez92/BetwinnerFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenez92;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetwinnerFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
