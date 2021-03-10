7fcf661d44c2eda26a26b6c7720edf7651e9c46b
Daniel <daniel.kaminski@smartive.app>
2021-03-10 14:37:13 +0100

    Prepare to deployment

--- src/main/java/com/kenez92/BetwinnerFrontApplication.java
+++ src/main/java/com/kenez92/BetwinnerFrontApplication.java
@@ -2,12 +2,25 @@ package com.kenez92;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class BetwinnerFrontApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(BetwinnerFrontApplication.class, args);
+//    }
+//
+//}
 
 @SpringBootApplication
-public class BetwinnerFrontApplication {
-
+public class BetwinnerFrontApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BetwinnerFrontApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BetwinnerFrontApplication.class);
+    }
 }
